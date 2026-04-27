01eba899485e3bb663e70be39ea24e7758a80d31
Gavin Smith <me@gavinhsmith.com>
2026-04-27 13:50:12 -0500

    [bot] Removed old testing commands

--- src/bot/commands/registry.ts
+++ src/bot/commands/registry.ts
@@ -1,13 +1,10 @@
 
-import { helloCommand, handleHello } from "./hello";
-import { randomCommand, handleRandom } from "./random";
 import { registerCommand, handleRegister } from "./register";
 import { createCommand, handleCreate } from "./create";
 import { groupCommand, handleGroup } from "./group";
 import { joinCommand, handleJoin } from "./join";
 import { kickCommand, handleKick } from "./kick";
 import { manageCommand, handleManage } from "./manage";
-import { flipCoinCommand, handleFlipCoin } from "./flipCoin";
 import { githubCommand, handleGithub } from "./github";
 import { leaveCommand, handleLeave } from "./leave";
 
@@ -16,17 +13,14 @@ import { leaveCommand, handleLeave } from "./leave";
  * - commandDefinitions: used by registerCommands.ts to sync with Discord
  * - commandHandlers: used at runtime to route interactions by name
  */
-export const commandDefinitions = [helloCommand, randomCommand, registerCommand, createCommand, groupCommand, joinCommand, kickCommand, manageCommand, flipCoinCommand, githubCommand, leaveCommand];
+export const commandDefinitions = [registerCommand, createCommand, groupCommand, joinCommand, kickCommand, manageCommand, githubCommand, leaveCommand];
 export const commandHandlers = new Map([
-    [helloCommand.name, handleHello],
-    [randomCommand.name, handleRandom],
     [registerCommand.name, handleRegister],
     [createCommand.name, handleCreate],
     [groupCommand.name, handleGroup],
     [joinCommand.name, handleJoin],
     [kickCommand.name, handleKick],
     [manageCommand.name, handleManage],
-    [flipCoinCommand.name, handleFlipCoin],
     [githubCommand.name, handleGithub],
     [leaveCommand.name, handleLeave]
 ]);
